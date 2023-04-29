b0977a573595372bc847cdcfabb85dca5e76c7cd
Keray18 <keshavbahoray18@gmail.com>
2023-04-29 23:10:46 +0530

    V~4.0

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -60,7 +60,7 @@ const Form = () => {
         formData.append('picturePath', values.picture.name)
 
         const savedUserResponse = await fetch(
-            "http://localhost:3001/auth/register",
+            "https://mern-socially-backend.onrender.com/auth/register",
             {
                 method: "POST",
                 body: formData,
