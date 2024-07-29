016a1dc48da407372bd0be2bfe9a4e3b9fb90b3f
Keshav Bahoray <110491299+Keray18@users.noreply.github.com>
2024-07-29 15:27:06 +0530

    Update UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -50,7 +50,7 @@ const UserWidget = ({ userId, picturePath }) => {
             <FlexBetween
                 gap="0.5rem"
                 pb="1.1rem"
-                onClick={ () => navigate(`https://socially-4p72.onrender.com/profile/${ userId }`)}
+                onClick={ () => navigate(`profile/${ userId }`)}
             >
                 <FlexBetween gap="1rem">
                     <UserImage image={picturePath} />
